a707250abf6f6a4c9eb521dc8135df1198e5bfdb
rayanadir <rayan.dahmena@gmail.com>
2022-06-11 21:34:43 +0200

    render Modal component

--- example/src/index.js
+++ example/src/index.js
@@ -2,6 +2,6 @@ import './index.css'
 
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './App'
+import Modal from './Modal.jsx'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(<Modal />, document.getElementById('root'))
